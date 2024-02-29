d89ca666fa1ff3fcd649c1178065eb282dde3464
ardaalkan <ardaaa.alk@gmail.com>
2024-02-29 14:16:54 +0300

    fake fetchs and bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -132,26 +132,7 @@ module.exports = {
           'Plus Jakarta Text',
         ]
       },
-      // borderWidth: {
-      //   '0.5': '0.5px',
-      // },
     },
-    // screens: {
-    //   '2xl': { 'max': '1535px' },
-    //   // => @media (max-width: 1535px) { ... }
-
-    //   'xl': { 'max': '1279px' },
-    //   // => @media (max-width: 1279px) { ... }
-
-    //   'lg': { 'max': '1023px' },
-    //   // => @media (max-width: 1023px) { ... }
-
-    //   'md': { 'max': '767px' },
-    //   // => @media (max-width: 767px) { ... }
-
-    //   'sm': { 'max': '639px' },
-    //   // => @media (max-width: 639px) { ... }
-    // }
   },
   plugins: [require('@tailwindcss/forms'), require('preline/plugin')]
 }
