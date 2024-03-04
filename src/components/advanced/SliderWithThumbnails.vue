aeb62b7f9045a7de15931360b363cfb9c00dc560
ardaalkan <ardaaa.alk@gmail.com>
2024-03-04 09:12:04 +0300

    init home views/components

--- src/components/advanced/SliderWithThumbnails.vue
+++ src/components/advanced/SliderWithThumbnails.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div id="main-slider" class="splide">
-            <div class="splide__track">
+            <div class="splide__track ">
                 <ul class="splide__list">
                     <li class="splide__slide" v-for="(image, index) in images" :key="index">
                         <img :src="image.url" alt="Slide Image" />
@@ -10,7 +10,6 @@
                 </ul>
             </div>
         </div>
-
         <div id="thumbnail-slider" class="splide">
             <div class="splide__track">
                 <ul class="splide__list">
@@ -20,9 +19,48 @@
                 </ul>
             </div>
         </div>
+        <form class="md:relative md:max-w-[798px] mx-auto fixed bottom-0">
+            <div class="flex flex-row">
+                <div class="bg-white mr-4 py-3 px-8 rounded-t-md cursor-pointer">
+                    Feribot Bileti
+                </div>
+                <div class="mr-4 bg-gray-800 py-3 px-8 rounded-t-md text-white cursor-pointer">
+                    Rezervasyon Ara
+                </div>
+            </div>
+            <div class="bg-white h-20 rounded-b-md rounded-tr-md flex items-center pl-4 w-full justify-between p-12">
+                <div class="p-2">
+                    <div>Nereden?</div>
+                    <div>Kuşadası</div>
+                </div>
+                <div class="p-2">
+                    icons
+                </div>
+                <div class="p-2">
+                    <div>Nereye?</div>
+                    <div>Samos</div>
+                </div>
+                <div class="p-2">
+                    <div>Bilet Tipi</div>
+                    <div>Gidiş - Dönüş</div>
+                </div>
+                <div class="p-2">
+                    <div>Gidiş-Dönüş</div>
+                    <div>Tarih Seçin</div>
+                </div>
+                <div class="p-2">
+                    <div>Kişi Sayısı</div>
+                    <div>Kişi Seçin</div>
+                </div>
+                <div
+                    class="w-[59px] h-[59px] bg-slate-200 rounded-full flex flex-row justify-center items-center cursor-pointer">
+                    İkon
+                </div>
+            </div>
+        </form>
     </div>
 </template>
-  
+
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import Splide from '@splidejs/splide';
@@ -38,7 +76,6 @@ const images = ref<Image[]>([
     { url: 'https://via.placeholder.com/1240x630', thumbnail: 'https://via.placeholder.com/124x63', title: 'Slide 1' },
     { url: 'https://via.placeholder.com/1240x630', thumbnail: 'https://via.placeholder.com/124x63', title: 'Slide 2' },
     { url: 'https://via.placeholder.com/1240x630', thumbnail: 'https://via.placeholder.com/124x63', title: 'Slide 3' },
-    // Add more images as needed
 ]);
 
 let main: Splide;
@@ -46,11 +83,12 @@ let thumbnails: Splide;
 
 const goToSlide = (index: number) => {
     main.go(index);
+    console.log(index, 'index')
 };
 
 onMounted(() => {
     main = new Splide('#main-slider', {
-        type: 'loop',
+        type: 'slide',
         heightRatio: 0.5,
         pagination: false,
         arrows: false,
@@ -60,7 +98,6 @@ onMounted(() => {
     thumbnails = new Splide('#thumbnail-slider', {
         rewind: true,
         fixedWidth: 211,
-        fixedHeight: 106,
         isNavigation: true,
         gap: 10,
         pagination: false,
@@ -79,7 +116,7 @@ onMounted(() => {
     }).mount();
 
     thumbnails.on('click', (slide: any) => {
-        main.go(slide.index);
+        goToSlide(slide.index); // Bu satırı değiştirdim.
     });
 
     main.on('moved', (newIndex: number) => {
@@ -87,54 +124,24 @@ onMounted(() => {
     });
 });
 </script>
-  
-  
+
+
+
 <style scoped>
-.thumbnails {
-    display: flex;
-    margin: 1rem auto 0;
-    padding: 0;
-    justify-content: center;
-}
-
-.thumbnail {
-    width: 70px;
-    height: 70px;
-    overflow: hidden;
-    list-style: none;
-    margin: 0 0.2rem;
-    cursor: pointer;
-}
-
-.thumbnail img {
-    width: 100%;
-    height: auto;
-}
 
 .splide {
     padding: 10px 0;
     display: flex;
     flex-direction: row;
-    width: 100%
+    width: 100%;
+    margin: auto
 }
 
 .splide__list {
     display: flex;
-    justify-content: center;
     align-items: center;
     width: 100%;
     flex-direction: row;
-}
-
-.splide__track {
-    width: 100%;
-}
-
-
-
-.splide__slide img {
-    width: 100%;
-    height: auto;
 }
 
 .splide__slide {
@@ -160,4 +167,13 @@ onMounted(() => {
     height: 2em;
     width: 4em;
 }
+
+.h-75 {
+    height: 75% !important;
+}
+
+.h-25 {
+    height: 25% !important;
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,279 @@
 <template>
   <div class="flex flex-col justify-center items-center m-auto relative">
+    <div class="w-full">
+      <div class="lg:px-28 md:px-16 sm:px-8">
+        <SliderWithThumbnails />
+      </div>
+      <section class="lg:px-28 md:px-16 sm:px-8">
 
-    <div class="w-full h-[223px]" />
+        <div class="relative w-full h-[494px] rounded-xl flex flex-col justify-between mb-10">
+          <div
+            class="absolute bottom-0 z-10 bg-white md:max-w-[551px] h-[466px] w-full p-7 md:ml-4 md:mb-4 rounded-xl flex flex-col justify-between">
+            <div class="flex flex-col">
+              <div
+                class="text-black text-base font-semibold font-['Plus Jakarta Sans'] leading-[18.46px] tracking-[2.96px] mb-[22px]">
+                KAMPANYA
+              </div>
+              <div
+                class="text-black lg:text-[42px] text-4xl font-bold font-['Plus Jakarta Sans'] leading-[48.47px] tracking-wide flex flex-row">
+                Kasım’da Kuşadası<br />Samos Feribot Bileti<br />€30! Her Cumartesi<br />ve Pazar.
+              </div>
+            </div>
+            <div class="flex flex-row">
+              <div
+                class="text-gray-800 text-base font-medium font-display leading-[24.86px] tracking-tight pb-[31px] mr-[10px]">
+                Tüm Turları Keşfet
+              </div>
+              <div class="w-7 h-7 p-1 bg-gray-800 rounded-full justify-center items-center inline-flex">
+                <div class="w-5 h-5 relative flex-col justify-start items-start flex"></div>
+              </div>
+            </div>
+          </div>
+          <img
+            src="https://images.unsplash.com/photo-1583062482795-d2bef78e9bc1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="asd" class="object-fill w-full h-full rounded-xl flip-horizontal" />
+        </div>
 
-    <div class="relative top-[-10rem] w-full lg:px-28 md:px-16 sm:px-8">
-      <SliderWithThumbnails />
+        <div class="flex flex-row gap-5 ">
+          <div
+            class="w-full flex flex-col bg-gradient-to-r from-blue-200 to-blue-400 h-[450px] rounded-xl pl-10 pt-10 overflow-hidden relative">
+            <div
+              class="text-black text-base font-semibold font-['Plus Jakarta Sans'] leading-[18.46px] tracking-[2.96px] mb-[14px]">
+              1 GÜNDE VİZE
+            </div>
+            <div class="text-black text-[38px] font-bold font-['Plus Jakarta Sans'] leading-[43.85px] tracking-wide">
+              Schengen Vizenizi<br />Biz Alalım
+            </div>
+            <img src="../assets//embeddedImages//mainPageTicketImage.png" alt="embedded"
+              class="flip-horizontal absolute h-[650px] left-64 -bottom-48 z-auto" />
+          </div>
+          <div class="relative w-full h-[450px] rounded-xl flex flex-col justify-between">
+            <div class="absolute bottom-0 z-10 bg-white md:w-2/3 w-full p-7 md:ml-4 md:mb-4 rounded-xl">
+              <div class="flex flex-col justify-between h-full">
+                <div
+                  class="text-black text-base font-semibold font-['Plus Jakarta Sans'] leading-[18.46px] tracking-[2.96px] mb-4">
+                  SAMOS TURLARI</div>
+                <div
+                  class="text-black text-[38px] font-bold font-['Plus Jakarta Sans'] leading-[43.85px] tracking-wide mb-[34px]">
+                  Türkçe Rehberli<br />Samos Turu €25</div>
+                <div
+                  class=" pt-[6px] pb-[7px] pl-3 pr-[7px] w-[125px] text-black text-[15px] font-medium font-display bg-slate-200 rounded-md">
+                  Tüm Turlar
+                </div>
+              </div>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1583061386694-e364c84ba31d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="asd" class="object-fill w-full h-full rounded-xl flip-horizontal" />
+          </div>
+        </div>
+      </section>
+
+      <section class="lg:px-28 md:px-16 sm:px-8">
+        <div class="text-black text-[42px] font-medium font-display tracking-wide mt-[76px]">Hergün çıkışlı<br />Samos
+          Turları</div>
+        <div class="w-full flex flex-row justify-between items-center mt-[37px]">
+          <div
+            class="text-black text-lg font-normal font-display tracking-tight flex flex-row justify-center items-center">
+            Eşsiz tarihi Yunan
+            mimarisi, görülmeye değer mekanlar.</div>
+          <div class="flex flex-row  items-center justify-centerh-8 bg-slate-200 rounded-lg border pr-[9px]">
+            <div
+              class="text-black text-[15px] font-medium font-display flex justify-center items-center mt-1 mr-1 pl-3 py-[6px] cursor-pointer">
+              Hepsini Gör
+            </div>
+            <IconArrowUpRight />
+          </div>
+        </div>
+
+        <div class="flex flex-row gap-5 md:mb-[120px] mb-20">
+          <!-- sections -->
+          <div class="w-[400px] h-[510px] rounded-xl border border-gray-200 mt-12 first:bg-slate-100">
+            <img
+              src="https://images.unsplash.com/photo-1498307833015-e7b400441eb8?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="asd" class="object-fill w-full max-h-[241px] flip-horizontal p-5" />
+            <div class="px-5 pt-2">
+              <div class="text-black text-[23px] font-semibold font-['Plus Jakarta Sans'] tracking-wide pb-[38px]">1
+                Gece
+                2 Gün
+              </div>
+              <div class=" text-black text-base font-normal font-display leading-snug tracking-tight">
+                Feribot biletleri, liman vergileri, oda/kahvaltı otel konaklaması, kah..</div>
+              <div
+                class="flex flex-row items-center justify-between text-right text-black text-lg font-medium font-display leading-[18px] tracking-tight mt-7">
+                <span>
+                  €74’dan başlayan fiyatlar
+                </span>
+                <span>
+                  <div class="flex flex-row items-center justify-center p-5 bg-white rounded-full">
+                    <IconShare2 />
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+          <!-- sections -->
+          <div class="w-[400px] h-[510px] rounded-xl border border-gray-200 mt-12 first:bg-slate-100">
+            <img
+              src="https://images.unsplash.com/photo-1520175480921-4edfa2983e0f?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="asd" class="object-fill w-full max-h-[241px] flip-horizontal p-5 rounded-md" />
+            <div class="px-5 pt-2">
+              <div class="text-black text-[23px] font-semibold font-['Plus Jakarta Sans'] tracking-wide pb-[38px]">2
+                Gece
+                3 Gün
+              </div>
+              <div class=" text-black text-base font-normal font-display leading-snug tracking-tight">
+                Feribot biletleri, liman vergileri, oda/kahvaltı otel konaklaması, kah..</div>
+              <div
+                class="flex flex-row items-center justify-between text-right text-black text-lg font-medium font-display leading-[18px] tracking-tight mt-7">
+                <span>
+                  €99’dan başlayan fiyatlar
+                </span>
+                <span>
+                  <div class="w-[61px] h-[61px] bg-white rounded-full"></div>
+                </span>
+              </div>
+            </div>
+          </div>
+          <!-- sections -->
+          <div class="w-[400px] h-[510px] rounded-xl border border-gray-200 mt-12 first:bg-slate-100">
+            <img
+              src="https://images.unsplash.com/photo-1492979423821-7193e8938424?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="asd" class="object-fill w-full max-h-[241px] flip-horizontal p-5 rounded-md" />
+            <div class="px-5 pt-2">
+              <div class="text-black text-[23px] font-semibold font-['Plus Jakarta Sans'] tracking-wide pb-[38px]">3
+                Gece
+                4 Gün
+              </div>
+              <div class=" text-black text-base font-normal font-display leading-snug tracking-tight">
+                Feribot biletleri, liman vergileri, oda/kahvaltı otel konaklaması, kah..</div>
+              <div
+                class="flex flex-row items-center justify-between text-right text-black text-lg font-medium font-display leading-[18px] tracking-tight mt-7">
+                <span>
+                  €124’dan başlayan fiyatlar
+                </span>
+                <span>
+                  <div class="w-[61px] h-[61px] bg-white rounded-full"></div>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+      <section>
+        <div class="w-full h-[935px] bg-slate-200 flex flex-col items-center mb-[120px]">
+          <div class=" text-black text-[42px] font-medium font-display tracking-wide mt-[100px] mb-7">Samos
+            Adasında<br />neler yapabilirim?</div>
+          <div
+            class="w-[534px] text-center text-neutral-600 text-base font-normal font-display leading-[24.86px] tracking-tight">
+            Marzipan sweet roll gingerbread oat cake cheesecake pie. Pie macaroon sweet liquorice topping.</div>
+          <div class="mt-[107px] flex flex-col">
+            <div class="flex flex-row mb-5 flex-wrap justify-center">
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+
+            </div>
+            <div class="flex flex-row  mb-5 flex-wrap justify-center">
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+            </div>
+            <div class="flex flex-row justify-center flex-wrap ">
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+              <div
+                class="p-3 pr-5 flex flex-row  rounded-[128px] border border-neutral-200 items-center cursor-pointer bg-white mr-5">
+                <img class="w-[66px] h-[66px] rounded-full ml-[17px]" alt="Slide Image"
+                  src="https://via.placeholder.com/66x66" />
+                <div class="text-gray-800 text-lg font-medium font-display ml-[26px]">Macera Turu</div>
+              </div>
+            </div>
+          </div>
+          <div class="mt-[89px] mb-[119px] flex flex-row">
+            <div class="w-[333px] text-neutral-600 text-base font-normal font-display leading-[24.86px] tracking-tight">
+              Ve daha fazlası için blog yazılarımızı okuyun.</div>
+            <div class="flex flex-row">
+              <div class="text-gray-800 text-base font-medium font-display leading-[24.86px] tracking-tight mr-3">
+                Samos’u
+                Keşfet
+              </div>
+              <div
+                class="flex items-center justify-center w-7 h-7 p-1 flex-shrink-0 rounded-full bg-gray-800 cursor-pointer">
+                <IconFooter />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import SliderWithThumbnails from '@/components/advanced/SliderWithThumbnails.vue';
+import IconArrowUpRight from '@/components/icons/IconArrowUpRight.vue';
+import IconShare2 from '@/components/icons/IconShare2.vue';
+import IconFooter from '@/components/icons/IconFooter.vue';
+// import IconArrowUpRight from '@/components/icons/IconArrowUpRight.vue';
+// import IconSamosaVector from '@/components/icons/IconSamosaVector.vue';
 </script>
 
 <style scoped></style>
+
+
+<!-- <div class="lg:w-[551px] lg:h-[466px] w-full bg-white p-[35px] rounded-lg flex flex-col justify-between">
+  
+</div> -->
