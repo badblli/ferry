8279f1c30ddb0677a2054712a92c90fe227541c3
badblli <bsnradblli@gmail.com>
2024-07-30 12:44:42 +0300

    chore: Update API key in .env file

---
+++ src/views/DynamicPages/TermOfUse.vue
@@ -0,0 +1,29 @@
+<template>
+     <div>
+          <h1>Terms of Use</h1>
+          <p>{{ terms }}</p>
+     </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+     name: 'TermOfUse',
+     setup() {
+          const terms = ref('')
+
+          // Fetch terms of use from API or set it manually
+          // Example:
+          // terms.value = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+          return {
+               terms
+          }
+     }
+})
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
